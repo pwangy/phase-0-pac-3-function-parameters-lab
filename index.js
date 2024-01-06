c9175d327d99f788d03d7927ae8c31088d2533f0
pwangy <blipsandclicks@gmail.com>
2024-01-05 19:35:16 -0800

    all tests pass

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function introduction(name) {
 function introductionWithLanguage(name, language) {
   return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
+
+function introductionWithLanguageOptional(name, language = 'JavaScript') {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
